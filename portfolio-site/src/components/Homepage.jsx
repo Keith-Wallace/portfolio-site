309a48600c277348e7194d0a78f078ae4ced2622
Keith Wallace <keith.t.wallace@gmail.com>
2026-03-19 12:52:47 -0400

    Create Vite + React + JavaScript project

---
+++ portfolio-site/src/components/Homepage.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function Homepage() {
+  return (
+    <>
+        <h1>Keith Wallace</h1>
+        <h2>Staff Frontend Engineer</h2>
+        <p>
+            <ul>
+                <li>Front end Architecture</li>
+                <li>Design Systems</li>
+                <li>Developer Experience</li>
+            </ul>
+        </p>
+        <p>
+            <a href="tel:917-921-8436">917-921-8436</a>
+            <br />
+            <a href="mailto:keith@wallace.com">keith@wallace.com</a>
+            <br />
+            <a href="https://linkedin.com/in/keithtwallace/" target="_blank">LinkedIn</a>
+        </p>
+    </>
+  )
+}
+
+export default Homepage
